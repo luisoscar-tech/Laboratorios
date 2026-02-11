67b723bd60cbae4e7de2518b6b1cc79622f1b88a
luisoscar-tech <luisoscartv@gmail.com>
2026-02-10 22:02:34 -0600

    Update Lab 3.cs

--- Lab 3.cs
+++ Lab 3.cs
@@ -13,23 +13,14 @@ namespace Laboratorio3
         // Punto de inicio de la ejecución del programa.
         static void Main(string[] args)
         {
-            // Write:
-            // Muestra un mensaje en pantalla SIN hacer salto de línea.
             Console.Write("Este mensaje se muestra ");
-            // WriteLine:
-            // Muestra un mensaje y luego hace un salto de línea.
             Console.WriteLine("en la misma línea.");
-            // El cursor pasa automáticamente a la siguiente línea
-            Console.WriteLine("Este mensaje aparece en una nueva línea.");
-            // Variable de tipo texto
+            Console.WriteLine("Este mensaje aparece en una nueva línea.")
             string nombre;
-            // Solicitud de datos usando Write (el cursor se queda en la misma línea)
             Console.Write("Ingrese su nombre: ");
-            // ReadLine:
-            // Lee una línea completa de texto ingresada por el usuario.
             nombre = Console.ReadLine();
             // Uso de la variable en una salida
-            Console.WriteLine("Hola " + nombre);
+            Console.WriteLine("Nombre del estudiante " + nombre);
 
             // Mensaje informativo
             Console.WriteLine("Presione cualquier tecla para continuar...");
